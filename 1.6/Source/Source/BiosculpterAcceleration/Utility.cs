b937e672215fdf02bcb53e4406a9e056979e7c92
uhj1250 <uhj1250@users.noreply.github.com>
2025-07-12 18:37:04 +0900

    1.6

---
+++ 1.6/Source/Source/BiosculpterAcceleration/Utility.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using Verse;
+
+
+namespace BiosculpterAcceleration
+{
+    public static class Utility
+    {
+        public static float Normalize(this float val, float min, float max)
+        {
+            return (val - min) / (max - min);
+        }
+
+        public static float DeNormalize(this float val, float min, float max)
+        {
+            return val * (max - min) + min;
+        }
+    }
+}
---
+++ 1.6/Source/Source/Module_FasterBioPod/Harmony.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using HarmonyLib;
+using BiosculpterAcceleration;
+using FasterBiosculpterPod;
+
+namespace BiosculpterAcceleration.Module_FasterBioPod
+{
+    [StaticConstructorOnStartup]
+    internal static class Init
+    {
+        static Init()
+        {
+            Harmony harmony = new Harmony("BiosculpterAcceleration");
+            harmony.PatchAll();
+        }
+    }
+
+    [HarmonyPatch(typeof(Patch_CompBiosculpterPod))]
+    public static class Patch_Patch_CompBiosculpterPod
+    {
+        public static Settings settingcache = LoadedModManager.GetMod<FasterBiosculpterPod.FasterBiosculpterPod>().GetSettings<Settings>();
+        
+
+        [HarmonyPatch("get_NutritionRequired")]
+        [HarmonyPostfix]
+        public static void NutritionRequired(ref float __result)
+        {
+            __result = settingcache.NutritionRequired;
+        }
+
+
+
+
+    }
+
+
+
+
+}
